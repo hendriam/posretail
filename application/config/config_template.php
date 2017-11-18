c6f122692cf131914c1d3395c377be0d83a43ede
Hendri <elhenry18@gmail.com>
2017-11-18 10:58:45 +0700

    Update config_template on base_url

--- application/config/config_template.php
+++ application/config/config_template.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['base_url'] = 'http://localhost/posapps/';
+$config['base_url'] = 'http://localhost/posretail/';
 $config['index_page'] = '';
 $config['uri_protocol']	= 'REQUEST_URI';
 $config['url_suffix'] = '';
